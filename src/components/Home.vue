63aff2e4b9758193b928f81e0286b39e0000c798
SaurabhApraj <saurabhapraj7@gmail.com>
2020-07-14 23:16:36 +0530

    Added Events

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,8 @@
     <h1>Hello from Home Components.</h1>
     <h2>{{data}}</h2>
     <h2>{{msg}}</h2>
+    <button v-on:click="hello">onClick</button>
+    <button v-on:mouseover="mouse">mouseOver</button>
   </div>
 </template>
 
@@ -12,6 +14,14 @@ export default {
   props: {
     data: String,
     msg: String
+  },
+  methods: {
+    hello() {
+      alert("Vue");
+    },
+    mouse() {
+      alert("Mouse over");
+    }
   }
 };
 </script>
